437e2426a8ffea7b580f90da118c63d991f6c180
IgorgabrielM <igorgabprofissional@gmail.com>
2022-07-09 02:21:08 -0300

    add a simple responsivit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '100px',
+      // => @media (min-width: 640px) { ... }
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 }
